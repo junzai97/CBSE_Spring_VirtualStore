b8b9d29e1c469b8012b2d7d20bb8211e916b1543
Clement Saw Kiat Chun <cskiat06@gmail.com>
2020-12-28 11:21:05 +0800

    Init commit

---
+++ src/Product.java
@@ -0,0 +1,20 @@
+public class Product {
+    String name;
+    double price;
+
+    public Product() {
+    }
+
+    public Product(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public double getPrice() {
+        return this.price;
+    }
+}
